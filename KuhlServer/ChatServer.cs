32b7a9bb012a83c946bfe81a00a1b8bda240afba
H3adsho0ot <niklas.roll123@gmail.com>
2014-10-14 14:45:11 +0200

    Created Projekt

---
+++ KuhlServer/ChatServer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunicationLibrary;
+using System.Net.Sockets;
+using System.Collections;
+
+namespace KuhlServer
+{
+    class ChatServer
+    {
+
+        public Server Server;
+        public Hashtable User = new Hashtable();
+
+        public ChatServer()
+        {
+            Server = new Server();
+            Server.start(45454);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Server started on Port 45454");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Server.onConnect += new ServerConnectHandler(ConnectEvent);
+            Server.onDisconnect += new ServerDisconnectHandler(DisconnectEvent);
+            Server.onReceive += new ServerReceiveHandler(ReceiveEvent);
+        }
+
+        public void ConnectEvent(TcpClient client)
+        {
+            Console.WriteLine("Connected: " + client.Client.GetHashCode());
+            Server.send(client, "welcome", "");
+        }
+
+        public void DisconnectEvent(TcpClient client)
+        {
+            Console.WriteLine("Client " + User[client.Client.GetHashCode()] + "(" + client.Client.GetHashCode() + ") disconnected from Server");
+            User.Remove(client.Client.GetHashCode());
+        }
+
+        public void ReceiveEvent(TcpClient client, String type, String message)
+        {
+            switch(type)
+            {
+                case "message":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Client " + User[client.Client.GetHashCode()] + "(" + client.Client.GetHashCode() + ") send message: " + message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    message = User[client.Client.GetHashCode()] + ": " + message;
+                    Server.sendToAll("message", message);
+                    break;
+                case "name":
+                    User[client.Client.GetHashCode()] = message;
+                    Console.WriteLine("Client " + client.Client.GetHashCode() + " is connected as " + message);
+                    break;
+            }
+        }
+    }
+}
---
+++ KuhlServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace KuhlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatServer ChatServer = new ChatServer();
+            while (true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "/clients":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Connected clients");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        foreach (DictionaryEntry entry in ChatServer.User)
+                        {
+                            Console.WriteLine("{1}, {0}", entry.Key, entry.Value);
+                        }
+                        Console.WriteLine("----------------");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case "/exit":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Sevrer is shutting down");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        ChatServer.Server.stop();
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
